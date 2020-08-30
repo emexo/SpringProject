23dff42916fd8da2cec68af133dc19cdbb053fdf
eMexo Technologies <49759592+emexo@users.noreply.github.com>
2020-08-30 16:18:05 +0530

    Update ApplicationServiceImpl.java
    Added retry logic

--- SpringMVCWithSpringBoot/src/main/java/com/emexo/service/ApplicationServiceImpl.java
+++ SpringMVCWithSpringBoot/src/main/java/com/emexo/service/ApplicationServiceImpl.java
@@ -1,14 +1,26 @@
 package com.emexo.service;
 
 import com.emexo.entity.Application;
+import com.emexo.entity.Release;
 import com.emexo.repository.ApplicationRepository;
+import com.emexo.repository.ReleaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
 
 @Service
+@EnableRetry
 public class ApplicationServiceImpl implements ApplicationService {
     @Autowired
     private ApplicationRepository applicationRepository;
+
+    @Autowired
+    private ReleaseRepository releaseRepository;
 
     @Override
     public Iterable<Application> listApplications() {
@@ -16,9 +28,18 @@ public class ApplicationServiceImpl implements ApplicationService {
     }
 
     @Override
-    public void save(Application application) {
+    @Retryable(value = {SQLException.class }, maxAttempts = 3)
+    @Transactional
+    public void save(Application application) throws InterruptedException {
         applicationRepository.save(application);
+        Release release = new Release();
+        release.setDescription("Test");
+        //release.setDescription("Test Rollbackoooooooooooooooooooooooooooooooooooooooooooo");
+        releaseRepository.save(release);
     }
 
-
+    @Recover
+    public String recover(SQLException t){
+        return "Service recovered from failure.";
+    }
 }
