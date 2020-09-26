d3af39a8171d86f7cd6167a975085ec132842a22
eMexo Technologies <49759592+emexo@users.noreply.github.com>
2020-09-26 16:08:37 +0530

    Added controller for application and ticket

---
+++ RestfulApiUsingSpringBoot/src/main/java/com/emexo/controller/ApplicationsController.java
@@ -0,0 +1,39 @@
+package com.emexo.controller;
+
+import com.emexo.entity.Application;
+import com.emexo.exception.ApplicationNotFoundException;
+import com.emexo.service.ApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/application")
+public class ApplicationsController {
+    @Autowired
+    private ApplicationService applicationService;
+
+    @GetMapping("/applications")
+    public ResponseEntity<List<Application>> getAllApplications() {
+        List<Application> list = applicationService.listApplications();
+        return new ResponseEntity<List<Application>>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/application/{id}")
+    public ResponseEntity<Application> getApplication(@PathVariable("id") long id) {
+        try {
+            return new ResponseEntity<Application>(applicationService.findApplication(id), HttpStatus.OK);
+        } catch (ApplicationNotFoundException exception) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Application Not Found");
+        }
+    }
+
+    @PostMapping("/applicationsave")
+    public void submitForm(@RequestBody  Application application) {
+        applicationService.save(application);
+    }
+}
---
+++ RestfulApiUsingSpringBoot/src/main/java/com/emexo/controller/TicketsController.java
@@ -0,0 +1,26 @@
+package com.emexo.controller;
+
+import com.emexo.entity.Ticket;
+import com.emexo.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ticket")
+public class TicketsController {
+    @Autowired
+    private TicketService ticketService;
+
+
+    @GetMapping("/tickets")
+    public ResponseEntity<List<Ticket>> getAllTickets() {
+        List<Ticket> list = ticketService.listTickets();
+        return new ResponseEntity<List<Ticket>>(list, HttpStatus.OK);
+    }
+}
