2827f63541243bc1893230a10a498e19e5db489d
eMexo Technologies <49759592+emexo@users.noreply.github.com>
2022-10-09 09:18:32 +0530

    Update ApplicationControllerTest.java

--- RestfulApiUsingSpringBoot/src/test/java/com/emexo/controller/ApplicationControllerTest.java
+++ RestfulApiUsingSpringBoot/src/test/java/com/emexo/controller/ApplicationControllerTest.java
@@ -1,14 +1,11 @@
-package com.emexo.controller;
+package com.bugtracking.controller;
 
-import com.emexo.entity.Application;
-import com.emexo.service.ApplicationService;
-import com.emexo.service.ApplicationServiceTest;
+import com.bugtracking.entity.Application;
+import com.bugtracking.service.ApplicationService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,7 +13,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -26,13 +23,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ApplicationControllerTest {
@@ -41,6 +39,7 @@ public class ApplicationControllerTest {
 
     @MockBean
     ApplicationService applicationService;
+
 
     @Test
     public void testGetAllApplications() throws Exception {
@@ -90,7 +89,6 @@ public class ApplicationControllerTest {
 
         when(applicationService.listApplications()).thenThrow(new NullPointerException());
 
-        //Create a post request with an accept header for application\json
         RequestBuilder requestBuilder = MockMvcRequestBuilders
                 .get("/api/v1/applications")
                 .contentType(MediaType.APPLICATION_JSON);
@@ -147,12 +145,101 @@ public class ApplicationControllerTest {
 
     @Test
     public void saveApplicationTest() throws Exception {
+        Application application = new Application();
+        application.setId(1l);
+        application.setName("LRI");
+        application.setOwner("JPMC");
+        application.setDescription("JPMC");
+        when(applicationService.save(any())).thenReturn(application);
+
         RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/api/v1/applications")
-                .content(asJsonString(new Application("LRI", "JPMC", "Liquidity Risk Management")))
+                .content(asJsonString(application))
                 .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON);
 
         mockMvc.perform(requestBuilder).andExpect(status().isOk());
 
+    }
+
+    @Test
+    public void saveApplicationTestWithException() throws Exception {
+        when(applicationService.save(any())).thenThrow(new NullPointerException());
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/api/v1/applications")
+                .content(asJsonString(new Application("LRI", "JPMC", "Liquidity Risk Management")))
+                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(requestBuilder).andExpect(status().isNotFound());
+
+    }
+
+    @Test
+    public void saveApplicationTestWithEmptyData() throws Exception {
+        Application application = new Application();
+        application.setId(1l);
+        application.setName("LRI");
+        application.setOwner("JPMC");
+        application.setDescription("JPMC");
+        when(applicationService.save(any())).thenReturn(null);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/api/v1/applications")
+                .content(asJsonString(application))
+                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(requestBuilder).andExpect(status().isInternalServerError());
+
+    }
+
+    @Test
+    public void updateApplicationTest() throws Exception {
+        Application application = new Application();
+        application.setId(1l);
+        application.setName("LRI");
+        application.setOwner("JPMC");
+        application.setDescription("JPMC");
+        when(applicationService.save(any())).thenReturn(application);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.put("/api/v1/applications")
+                .content(asJsonString(application))
+                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(requestBuilder).andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void updateApplicationTestWithException() throws Exception {
+        when(applicationService.save(any())).thenThrow(new NullPointerException());
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.put("/api/v1/applications")
+                .content(asJsonString(new Application(1,"LRI", "JPMC", "Liquidity Risk Management")))
+                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(requestBuilder).andExpect(status().isNotFound());
+
+    }
+
+    @Test
+    public void deleteApplication() throws Exception {
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .delete("/api/v1/applications?id=1")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+
+        //Assert that the return status is OK
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
+
+    @Test
+    public void deleteApplication1() throws Exception {
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .delete("/api/v1/applications?id=1")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(requestBuilder).andExpect(status().isOk());
     }
 
        public String asJsonString(final Object obj) {
