cac2b2474930eed6a7909658f31670961cb8b44d
eMexo Technologies <49759592+emexo@users.noreply.github.com>
2023-02-21 07:11:35 +0530

    Update ApplicationSecurity.java

--- SpringJwtAuthExample/src/main/java/com/emexo/ApplicationSecurity.java
+++ SpringJwtAuthExample/src/main/java/com/emexo/ApplicationSecurity.java
@@ -2,33 +2,39 @@ package com.emexo;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 import com.emexo.jwt.JwtTokenFilter;
 import com.emexo.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@EnableWebSecurity(debug = true)
-public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+@Configuration
+public class ApplicationSecurity {
 
-	@Autowired private UserRepository userRepo;
-	
-	@Autowired private JwtTokenFilter jwtTokenFilter;
+	@Autowired
+	private UserRepository userRepo;
+	@Autowired
+	private JwtTokenFilter jwtTokenFilter;
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(username -> userRepo.findByEmail(username)
-				.orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found.")));
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new UserDetailsService() {
+
+			@Override
+			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+				return userRepo.findByEmail(username)
+						.orElseThrow(
+								() -> new UsernameNotFoundException("User " + username + " not found"));
+			}
+		};
 	}
 
 	@Bean
@@ -36,31 +42,33 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		
-		http.authorizeRequests()
-				.antMatchers("/auth/login").permitAll()
-				.anyRequest().authenticated();
-		
-        http.exceptionHandling()
-                .authenticationEntryPoint(
-                    (request, response, ex) -> {
-                        response.sendError(
-                            HttpServletResponse.SC_UNAUTHORIZED,
-                            ex.getMessage()
-                        );
-                    }
-                );
-        
-		http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+	@Bean
+	public AuthenticationManager authenticationManager(
+			AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
 	}
 
-	@Override
 	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http.authorizeRequests()
+				.antMatchers("/auth/login", "/docs/**", "/users").permitAll()
+				.anyRequest().authenticated();
+
+		http.exceptionHandling()
+				.authenticationEntryPoint(
+						(request, response, ex) -> {
+							response.sendError(
+									HttpServletResponse.SC_UNAUTHORIZED,
+									ex.getMessage()
+							);
+						}
+				);
+
+		http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
 	}
 }
