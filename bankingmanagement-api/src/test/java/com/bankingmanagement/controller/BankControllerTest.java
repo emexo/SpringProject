6ad7220d0acafee16dd393759edcb1feddfdbf82
eMexo Technologies <49759592+emexo@users.noreply.github.com>
2023-03-26 09:39:18 +0530

    Update BankControllerTest.java

--- bankingmanagement-api/src/test/java/com/bankingmanagement/controller/BankControllerTest.java
+++ bankingmanagement-api/src/test/java/com/bankingmanagement/controller/BankControllerTest.java
@@ -4,6 +4,7 @@ import com.bankingmanagement.exception.BankDetailsNotFound;
 import com.bankingmanagement.model.BankDTO;
 import com.bankingmanagement.model.BranchDTO;
 import com.bankingmanagement.service.BankService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,6 +25,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -98,5 +101,94 @@ public class BankControllerTest {
         // compare the status code
         mockMvc.perform(requestBuilder).andExpect(status().isNotFound());
 
+    }
+
+    @Test
+    public void testGetBankByCode() throws Exception {
+        BankDTO bankDTO = new BankDTO();
+        bankDTO.setBankName("SBI");
+        bankDTO.setBankAddress("Bangalore");
+        List<BranchDTO> branchDTOList = new ArrayList<>();
+        BranchDTO branchDTO = new BranchDTO();
+        branchDTO.setBranchName("Electronic City");
+        branchDTO.setBranchAddress("Electronic City");
+        branchDTOList.add(branchDTO);
+        bankDTO.setBranchDTOList(branchDTOList);
+
+        when(bankService.findBankDetails(anyInt())).thenReturn(bankDTO);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/api/v1/banks/99")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(requestBuilder).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testSave() throws Exception {
+        BankDTO bankDTO = new BankDTO();
+        bankDTO.setBankName("SBI");
+        bankDTO.setBankAddress("Bangalore");
+        List<BranchDTO> branchDTOList = new ArrayList<>();
+        BranchDTO branchDTO = new BranchDTO();
+        branchDTO.setBranchName("Electronic City");
+        branchDTO.setBranchAddress("Electronic City");
+        branchDTOList.add(branchDTO);
+        bankDTO.setBranchDTOList(branchDTOList);
+
+        when(bankService.save(any())).thenReturn(bankDTO);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/api/v1/banks")
+                .content(asJsonString(bankDTO))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(requestBuilder).andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        BankDTO bankDTO = new BankDTO();
+        bankDTO.setBankName("SBI");
+        bankDTO.setBankAddress("Bangalore");
+        List<BranchDTO> branchDTOList = new ArrayList<>();
+        BranchDTO branchDTO = new BranchDTO();
+        branchDTO.setBranchName("Electronic City");
+        branchDTO.setBranchAddress("Electronic City");
+        branchDTOList.add(branchDTO);
+        bankDTO.setBranchDTOList(branchDTOList);
+
+        when(bankService.save(any())).thenReturn(bankDTO);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .put("/api/v1/banks")
+                .content(asJsonString(bankDTO))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(requestBuilder).andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        when(bankService.delete(anyInt())).thenReturn("Bank has been deleted");
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .delete("/api/v1/banks?code=99")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(requestBuilder).andExpect(status().isOk());
+    }
+
+    public String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
