fb82d38b26acfdd0910cb9476311b585b801275f
eMexo Technologies <49759592+emexo@users.noreply.github.com>
2023-06-30 07:39:25 +0530

    Delete bugtracking-api/src/main/java/com/bankingmanagement directory

--- bugtracking-api/src/main/java/com/bankingmanagement/repository/BankRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bankingmanagement.repository;
-
-import com.bankingmanagement.entity.Bank;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankRepository extends JpaRepository<Bank, Integer> {
-}
