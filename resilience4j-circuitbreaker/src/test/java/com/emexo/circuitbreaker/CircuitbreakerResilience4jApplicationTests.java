dbeb10e14b4f9209dda805b5c2a40148c4d5696b
eMexo Technologies <49759592+emexo@users.noreply.github.com>
2023-12-24 18:31:03 +0530

    Add files via upload

---
+++ resilience4j-circuitbreaker/src/test/java/com/emexo/circuitbreaker/CircuitbreakerResilience4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emexo.circuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitbreakerResilience4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
